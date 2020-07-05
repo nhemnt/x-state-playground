298896b53ec6bc9c06772f083afd98e80e4c7075
hemant negi <nhemnt@gmail.com>
2020-07-06 01:06:36 +0530

    add login form

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getUpdateStateHook = (key) => {
+    return {
+      email: setEmail,
+      password: setPassword,
+    }[key]
+  }
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    const setState = getUpdateStateHook(name);
+    setState(value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //login here
+  }
+
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       Login
-    </div>
+      <div>
+        <input type="text" name="email" value={email} onChange={handleOnChange} required />
+      </div>
+      <div>
+        <input type="password" name="password" value={password} onChange={handleOnChange} required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
